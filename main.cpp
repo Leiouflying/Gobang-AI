e68eedd98f1f498d14f5a71037d8b5486e8a4342
leiouflying <chendetao@live.cn>
2018-01-01 22:53:17 +0800

    解决编码问题

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#undef UNICODE
+#undef _UNICODE	//取消 Unicode 编码的宏定义，让整个项目以 MBCS 编码编译，以解决编码问题
 #include <iostream>
 #include <stdio.h>
 
 #define VERSION_X 0
 #define VERSION_Y 0
-#define VERSION_Z 2		//More detail https://www.geekdt.com/335.html
+#define VERSION_Z 1		//More detail https://www.geekdt.com/335.html
 
 using namespace std;
 
