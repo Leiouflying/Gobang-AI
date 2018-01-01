a230c8727835e24a036597ae1d8541738994bcba
leiouflying <chendetao@live.cn>
2018-01-01 16:11:04 +0800

    add version control

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+
+#define VERSION_X 0
+#define VERSION_Y 0
+#define VERSION_Z 2
 using namespace std;
+
+void version();
+
 int main()
 {
-	cout << "Hello World. This Project is developing...";
+	version();
+	cout << "Hello World. This Project is developing...\n";
 	system("pause");
 	return 0;
+}
+void version()
+{
+	printf("VERSION: %d.%d.%d\n", VERSION_X, VERSION_Y, VERSION_Z);
 }
