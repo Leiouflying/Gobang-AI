9d4fbfda71f1566537cb289986fc231a9ae872b0
leiouflying <chendetao@live.cn>
2018-01-03 00:54:13 +0800

    create loadimage_transparent() and update some format of the files

--- UPDATE LOG.md
+++ UPDATE LOG.md
@@ -1,10 +1,9 @@
-# Update Log
+# Update_Log
 
-## Ver:0.0.1
+## Ver:0.0.1	180101
 - Created the project
 - Defined the version//Read more detail: https://www.geekdt.com/335.html
 - Add update log
-- Update some comments of `version()`
 - Cancel the Unicode to slove the encoding format problem
 - Changed some naming method
 - Rebuilt the format of update log
--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <graphics.h>
+#include <easyx_ex.h>
+#include <conio.h>
+// 引用该库才能使用 TransparentBlt 函数
+#pragma comment( lib, "MSIMG32.LIB")
 
 #define VERSION_X 0
 #define VERSION_Y 0
@@ -11,13 +15,28 @@
 using namespace std;
 
 void version();
+void loadimage_transparent(const char *local, int width, int height, COLORREF color, int x, int y);
 
 int main()
 {
 	version();
-	cout << "Hello World. This Project is developing...\n";
+	initgraph(800, 600);
+	loadimage_transparent("C:\\test.bmp", 800, 800, 0x00ff00, 0, 0);
+
 	system("pause");
 	return 0;
+}
+void loadimage_transparent(const char *local, int width, int height, COLORREF color, int x, int y)//Attention:bmp file only!!!
+//目标路径,目标宽度,目标高度,透明色,目标位置x轴偏移量,目标位置y轴偏移量
+{
+	IMAGE img;
+	loadimage(&img, local, width, height);
+	// 获取绘图窗口和 IMAGE 对象的句柄，TransparentBlt() 函数需要用
+	HDC dstDC = GetImageHDC();
+	HDC srcDC = GetImageHDC(&img);
+	// 使用 Windows GDI 函数实现透明位图
+	TransparentBlt(dstDC, x, y, width, height, srcDC, x, y, width, height, color);// 最后一个参数是设置透明色
+	FlushBatchDraw(); //使 GDI 操作生效
 }
 void version()	//More detail https://www.geekdt.com/335.html
 {
